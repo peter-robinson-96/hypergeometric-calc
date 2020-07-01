fabc1000adb54f08b55fd8a96b7ac70cb02947b4
Peter Robinson <peter.frederick.robinson@gmail.com>
2020-07-01 13:11:30 +1200

    Add in sufficient code for exporting component

---
+++ client/components/InputField.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export class InputField extends React.Component {
+  render () {
+    return (
+      <>
+        <form>is this the title before the form? or the existing text in the form? we'll find out together!</form>
+        <button>Calculate</button>
+      </>
+    )
+  }
+}
